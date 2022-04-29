9fb63d400030eba856cbc5145541c43c641fa9e0
Ted Laderas <roshioshi@gmail.com>
2022-04-28 21:03:21 -0700

    Update tm.py

--- midi_machine/tm.py
+++ midi_machine/tm.py
@@ -7,8 +7,14 @@ class turing_machine:
     range = 2
 
     def __init__(self, steps, interval, prob):
+        #if steps == 0:
+        #    raise(TypeError("Steps should be greater than 0"))
+        if interval > steps:
+            raise(TypeError("Interval should be less than number of steps"))
         self.bits = [0 for i in range(0, steps) if True]
         self.gate = [0 for i in range(0, steps) if True]
+        if prob > 1:
+            raise(TypeError("Proability not in range"))
         self.prob = prob
         self.note = None
         self.trig = False
@@ -24,12 +30,18 @@ class turing_machine:
         self.prob = prob
         return(self)
 
+    def seed_bits(self):
+        self.bits = [1 if random.random() > self.prob else 0 for i in range(0,len(self.bits))]
+
     def set_gate(self, interval=3):
         if interval >= len(self.gate):
             interval = interval - 2
         self.gate = [1 if i % interval == 0 else 0 for i in range(0,len(self.gate))]
         self.gate[len(self.gate)-1] = 0
         return(self)
+
+    def set_bits(self, bit_string):
+        bit_string.split()
 
     def get_bits(self):
         bit_string = "".join([str(n) for n in self.bits])
@@ -41,25 +53,34 @@ class turing_machine:
     
     def tick(self):
         self.max = 1
-        feedback = self.bits.pop(0)
-        print(self.bits)
+        feedback = self.bits[0]
         acc = feedback
+        self.trig = False 
         for i in range(0, len(self.bits)-2):
+            self.bits[i] = self.bits[i+1]
             acc = (acc * 2) + self.bits[i]
             self.max = (self.max * 2) + 1
+            print(i)
             if self.bits[i] != 0 and self.gate[i] != 0:
-                trig = True
-        flip = random()
+                self.trig = True
+
+        print(self.trig)        
+        flip = random.random()
         flip_inv = 1 - flip
         if self.prob >= flip:
-            self.bits.append(int(feedback))
+            self.bits[len(self.bits)-1] = feedback
         else:
-            self.bits.append(randint(0,1))     
+            self.bits[len(self.bits)-1] = random.randint(0,1)     
         if self.trig:
-            self.note = (self.acc / self.max * self.range)
+            self.note = (acc / self.max * self.range)
         else:
             self.note = None
         return(self)
     
     def get_note(self):
         return self.note
+
+
+class test:
+    def __init__(self, age):
+        self.age = age
